f7f73a8a15b3007d25b09067326d2101255caefc
Mabongakev <kelvin.mabonga92@gmail.com>
2025-04-24 01:32:59 +0300

    use of class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,3 +13,39 @@ interface  Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)} ${lastName}`;
+}
+
+
+// Define an interface for the constructor
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+  
+// Define an interface for the class structure
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+  
+// Implement the class using the interfaces
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+  
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  
+  displayName(): string {
+    return this.firstName;
+  }
+}
