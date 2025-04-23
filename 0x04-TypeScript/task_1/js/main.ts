03c6eb6521fd1bd33b6df10e6ac3328af21baadf
Mabongakev <kelvin.mabonga92@gmail.com>
2025-04-24 01:15:23 +0300

    Task 2

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+interface  Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    yearsOfExperience?: number;
+    fullTimeEmployee: boolean;
+    location: string;
+    contract?: boolean;
+    [key: string]: any; 
+}
