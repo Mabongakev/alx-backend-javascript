8b0d2738a448d1de94e9e03cc63e0ef263e0c0a3
Mabongakev <kelvin.mabonga92@gmail.com>
2025-06-13 23:00:04 +0300

    Task 0 revised

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,16 @@
-function calculateNumber(a, b){
-    return Math.round(a) + Math.round(b);
+#!/usr/bin/env node
+
+const Utils = require('./utils');
+
+/**
+ * Return the integer sum of two numbers.
+ * @param a {number} - The first number.
+ * @param b {number} - The second number.
+ * @returns {number} The sum of the two numbers rounded up.
+ */
+function calculateNumber(a, b) {
+  return Utils.calculateNumber('SUM', a, b);
 }
 
 module.exports = calculateNumber;
+
